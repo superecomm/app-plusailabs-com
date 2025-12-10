6e49ea1f20fb55bed44a0d3420007d3e24674ae3
superecomm <superecommcompany@gmail.com>
2025-12-10 11:22:20 -0600

    Remove circle container from NeuralBox avatar for cleaner look

--- components/viim/NeuralBox.tsx
+++ components/viim/NeuralBox.tsx
@@ -183,7 +183,7 @@ export function NeuralBox({
       );
     }
     return (
-      <div className="flex h-6 w-6 items-center justify-center rounded-full bg-gradient-to-br from-gray-900 to-gray-600 text-white flex-shrink-0">
+      <div className="flex h-6 w-6 items-center justify-center flex-shrink-0">
         <VIIMAnimation state={state} size="custom" customSize={24} container="none" />
       </div>
     );
